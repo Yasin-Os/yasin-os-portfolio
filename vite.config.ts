05b833c906cdba2cc1861d35bbfc22976917be1e
Yasin Adnan <yasin.info404@gmail.com>
2026-09-19 20:54:27 +0600

    Final vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
+    TanStackStartVite({
       server: { entry: "src/server.ts" }
     }),
     tailwindcss(),
